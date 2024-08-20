a2609603075506ceea6446fc1082085f0b86eb5c
Stone Casey <stonec@bauer.com>
2024-08-20 10:57:55 -0400

    intial commit

--- Snakegame/src/App.java
+++ Snakegame/src/App.java
@@ -1,6 +1,20 @@
+import java.awt.Color;
+import javax.swing.*;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        //asd
+        int boardWidth = 600;
+        int boardHeigth = boardWidth;
+        Color bgColor = Color.black;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeigth);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null); //opens in the center of the screen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setFocusable(true);
+        
     }
 }
